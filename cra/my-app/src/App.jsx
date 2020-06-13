be8d5eac12c50b51115532782dba2d11c9a0829d
Timur Yermakou <darkday3@mail.ru>
2020-06-13 13:56:41 +0300

    feat: add SCSS and styled components

--- cra/my-app/src/App.jsx
+++ cra/my-app/src/App.jsx
@@ -1,10 +1,25 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import styled from 'styled-components';
+import logo from './logo.svg';
+import './App.scss';
+
+const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${(props) => (props.primary ? 'palevioletred' : 'white')};
+  color: ${(props) => (props.primary ? 'white' : 'palevioletred')};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
 
 function App() {
   return (
     <div className="App">
+      <Button>Normal Button</Button>
+      <Button primary>Primary</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
